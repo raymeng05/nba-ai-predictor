16b19f7ca2192c3e6bd6b4adfc7982e258e3c42b
raymeng05 <raybenmeng@gmail.com>
2025-10-15 22:25:18 -0400

    feat: set up the springboot application

---
+++ src/main/java/com/nba/nba_predictor/NbaPredictorApplication.java
@@ -0,0 +1,13 @@
+package com.nba.nba_predictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaPredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaPredictorApplication.class, args);
+	}
+
+}
