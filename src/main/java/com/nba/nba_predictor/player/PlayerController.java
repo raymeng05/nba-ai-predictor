b78509f206eb2ac1dedc12c000275cf3451c712a
Zilin <hannahzweng@gmail.com>
2025-11-04 22:04:28 -0500

    updates to player service and rest api completed

---
+++ src/main/java/com/nba/nba_predictor/player/PlayerController.java
@@ -0,0 +1,78 @@
+package com.nba.nba_predictor.player;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// every method returns a domain object instead of a view
+
+@RequestMapping(path = "api/player")
+public class PlayerController {
+
+    private final PlayerService playerService;
+
+    @Autowired
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    // handles get requests -> gets players from the database
+    @GetMapping
+    public List<Player> getPlayers(
+            @RequestParam(required = false) String team,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String pos) {
+
+        if (team != null && pos != null) {
+            return playerService.getPlayersByTeamAndPosition(team, pos);
+        } else if (team != null) {
+            return playerService.getPlayersFromTeam(team);
+        } else if (name != null) {
+            return playerService.getPlayersByName(name);
+        } else if (pos != null) {
+            return playerService.getPlayersByPosition(pos);
+        } else {
+            return playerService.getPlayers();
+        }
+    }
+
+    // handles post requests -> adds player to database
+    @PostMapping
+    public ResponseEntity<Player> addPlayer(@RequestBody Player player){
+        Player newPlayer = playerService.addPlayer(player);
+        return new ResponseEntity<>(newPlayer, HttpStatus.CREATED);
+    }
+
+    // handles put requests -> updates a player in the repository
+    @PutMapping
+    public ResponseEntity<Player> updatePlayer(@RequestBody Player newPlayer) { // param newPlayer is the new info of the player we want to update
+        Player resultPlayer = playerService.updatePlayer(newPlayer);
+        if (resultPlayer != null) {
+            return new ResponseEntity<>(resultPlayer, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // handles delete requests -> deletes a player from the db
+    @DeleteMapping("/{name}")
+    public ResponseEntity<String> deletePlayer(@PathVariable String name) {
+        playerService.deletePlayer(name);
+        return new ResponseEntity<>("Successful deletion", HttpStatus.OK);
+    }
+
+    
+}
+
--- src/main/java/com/nba/nba_predictor/player/PlayerService.java
+++ src/main/java/com/nba/nba_predictor/player/PlayerService.java
@@ -1,9 +1,13 @@
 package com.nba.nba_predictor.player;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import jakarta.transaction.Transactional;
 
 @Component
 public class PlayerService {
@@ -18,7 +22,48 @@ public class PlayerService {
     public List<Player> getPlayers() {
         return playerRepository.findAll();
     }
+
     public List<Player> getPlayersFromTeam(String team) {
-        return playerRepository.findAll().stream().filter(player -> player.getTeam().equalsIgnoreCase(team)).toList();
+        return playerRepository.findAll().stream().filter(player -> team.equals(player.getTeam())).collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByName(String search) {
+        return playerRepository.findAll().stream().filter(player -> player.getName().toLowerCase().contains(search.toLowerCase())).collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByPosition(String search) {
+        return playerRepository.findAll().stream().filter(player -> player.getPosition().toLowerCase().contains(search.toLowerCase())).collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByTeamAndPosition(String team, String pos){
+        return playerRepository.findAll().stream().filter(player -> team.equals(player.getTeam()) && pos.equals(player.getPosition())).collect(Collectors.toList());
+    }
+
+    public Player addPlayer(Player player) {
+        playerRepository.save(player); // built into jpa repository as well
+        return player;
+    }
+
+    public Player updatePlayer(Player newPlayer) {
+        // find the player with the given name
+        Optional<Player> existingPlayer = playerRepository.findByName(newPlayer.getName()); // the container that holds the value
+
+        // update that player with the new info passed in
+        if (existingPlayer.isPresent()) {
+            Player playerToUpdate = existingPlayer.get(); // gets the actual value (Player)
+            playerToUpdate.setName(newPlayer.getName());
+            playerToUpdate.setTeam(newPlayer.getTeam());
+            playerToUpdate.setPosition(newPlayer.getPosition());
+            playerRepository.save(playerToUpdate);
+            return playerToUpdate;
+        }
+        return null;
+    }
+
+    // ensures all db operations within this method are a single unit
+    // maintains db integrity (if the delete fails, it will simply revert back, otherwise, transaction will go through)
+    @Transactional
+    public void deletePlayer(String name) {
+        playerRepository.deleteByName(name);
     }
 }
